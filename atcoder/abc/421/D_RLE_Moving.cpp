ab56b4ef157a70e5c11304bf6abdc3e525906049
sora takahashi <e.sora.takahashi@gmail.com>
2025-09-01 22:45:19 +0800

    Upsolving

--- atcoder/abc/421/D_RLE_Moving.cpp
+++ atcoder/abc/421/D_RLE_Moving.cpp
@@ -4,20 +4,103 @@ using namespace std;
 
 using ll = long long;
 using pii = pair<int, int>;
-using tiii = tuple<int, int, int>;
 
 void solve()
 {
-    pii x_t, y_t, x_a, y_a;
-    int n, m, l;
-    queue<tiii> t, a;
-
-    cin >> x_t.first >> y_t.first >> x_a.first >> y_a.first;
+    ll r_t, c_t, r_a, c_a;
+    cin >> r_t >> c_t >> r_a >> c_a;
+    ll n, m, l;
     cin >> n >> m >> l;
-    while(m --)
+    queue<pair<char, int>> q_t, q_a;
+    while (m--)
     {
-        char c; int a; cin >> c >> a;
+        char s;
+        int a;
+        cin >> s >> a;
+        q_t.emplace(s, a);
     }
+    while (l--)
+    {
+        char t;
+        int b;
+        cin >> t >> b;
+        q_a.emplace(t, b);
+    }
+    vector<char> d_t, d_a;
+    vector<int> move_len;
+    while (!q_a.empty())
+    {
+        auto & [s, a] = q_t.front();
+        auto & [t, b] = q_a.front();
+
+        d_t.emplace_back(s);
+        d_a.emplace_back(t);
+
+        int len = min(a, b);
+        move_len.emplace_back(len);
+        a -= len, b -= len;
+        if (a == 0) q_t.pop();
+        if (b == 0) q_a.pop();
+    }
+
+    // for (int i = 0; i < move_len.size(); ++i)
+    // {
+    //     cout << d_t[i] << " : " << d_a[i] << " -- " << move_len[i] << endl;
+    // }
+
+    ll ans = 0;
+    unordered_map<char, int> r, c;
+    r['U'] = -1, c['U'] = 0;
+    r['D'] = +1, c['D'] = 0;
+    r['L'] = 0, c['L'] = -1;
+    r['R'] = 0, c['R'] = +1;
+    for (int i = 0; i < move_len.size(); ++i)
+    {
+        if (r_a == r_t && c_a == c_t)
+        {
+
+            if (d_t[i] == d_a[i]) ans += move_len[i];
+        }
+        else if (r_a != r_t && c_a == c_t)
+        {
+            ll d = abs(r_t - r_a);
+            if (d % 2 == 0 && d / 2 <= move_len[i])
+            {
+                ll temp_r_t = r_t + r[d_t[i]] * d / 2;
+                ll temp_r_a = r_a + r[d_a[i]] * d / 2;
+                if (temp_r_t == temp_r_a) ans += 1;
+            }
+        }
+        else if (r_a == r_t && c_a != c_t)
+        {
+            ll d = abs(c_t - c_a);
+            if (d % 2 == 0 && d / 2 <= move_len[i])
+            {
+                ll temp_c_t = c_t + c[d_t[i]] * d / 2;
+                ll temp_c_a = c_a + c[d_a[i]] * d / 2;
+                if (temp_c_t == temp_c_a) ans += 1;
+            }
+        }
+        else
+        {
+            // 4. 判相交，使至少一个轴相同的步数，然后判断这个步数后是否相同
+            ll d = abs(r_t - r_a);
+            if (d <= move_len[i])
+            {
+                ll temp_r_t = r_t + r[d_t[i]] * d;
+                ll temp_c_t = c_t + c[d_t[i]] * d;
+                ll temp_r_a = r_a + r[d_a[i]] * d;
+                ll temp_c_a = c_a + c[d_a[i]] * d;
+                if (temp_r_t == temp_r_a && temp_c_t == temp_c_a) ans += 1;
+            }
+        }
+        r_t += r[d_t[i]] * move_len[i];
+        c_t += c[d_t[i]] * move_len[i];
+        r_a += r[d_a[i]] * move_len[i];
+        c_a += c[d_a[i]] * move_len[i];
+        // cout << r_t << " " << c_t << " " << r_a << " " << c_a << endl;
+    }
+    cout << ans << '\n';
 }
 
 int main()
--- atcoder/abc/421/F_Erase_between_X_and_Y.cpp
+++ atcoder/abc/421/F_Erase_between_X_and_Y.cpp
@@ -7,6 +7,46 @@ using pii = pair<int, int>;
 
 void solve()
 {
+    int q;
+    cin >> q;
+    vector<int> nxt(q + 1);
+    nxt[0] = -1;
+    for (int i = 1; i <= q; ++i)
+    {
+        int op;
+        cin >> op;
+        if (op == 1)
+        {
+            int x;
+            cin >> x;
+            nxt[i] = nxt[x];
+            nxt[x] = i;
+        }
+        else
+        {
+            int x, y;
+            cin >> x >> y;
+            ll sum_x = 0, sum_y = 0;
+            int i = nxt[x], j = nxt[y];
+            while (i != y && j != x)
+            {
+                if (i != -1) sum_x += i, i = nxt[i];
+                if (j != -1) sum_y += j, j = nxt[j];
+            }
+            if (i == y)
+            {
+                nxt[x] = y;
+                cout << sum_x << endl;
+            }
+            else
+            {
+                nxt[y] = x;
+                cout << sum_y << endl;
+            }
+        }
+        // for (int i = 0; i != -1; i = nxt[i]) cout << i << " ";
+        // cout << endl;
+    }
 }
 
 int main()
@@ -14,7 +54,7 @@ int main()
     ios::sync_with_stdio(false);  
     cin.tie(nullptr), cout.tie(nullptr);  
     int t = 1;
-    cin >> t;
+    // cin >> t;
     while (t--)
     {
         solve();
